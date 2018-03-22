977c0536a1ebe7aca959358944b6f894fbfd30e0
Matthieu SCARSET <m@matthieuscarset.com>
2018-03-22 05:50:17 -0400

    Update error msg + default location for debug

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from mod_api import WeatherAPI
 
 API_KEY = "38d423b88b9b7069"
+country = 'canada'
+state = 'quebec'
+city = 'montreal'
 
 def main():
-  country = str(input("What country do you want to forecast? ")).replace(" ", "+")
-  state = str(input("What state? ")).replace(" ", "+")
-  city = str(input("What city? ")).replace(" ", "+")
+  # country = str(input("What country do you want to forecast? ")).replace(" ", "+")
+  # state = str(input("What state? ")).replace(" ", "+")
+  # city = str(input("What city? ")).replace(" ", "+")
   try:
     _api = WeatherAPI(API_KEY)
     _raw = _api.query(country, state, city)
     try:
       _data = _raw['response']['results']
     except KeyError as e:
-      print("Error in the response (" + str(e) + ")")
+      print("No results for your search. Try something else.")
     else:
       _api.save(_data)
   except Exception as e:
